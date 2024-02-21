923e41734bb502bb68a2e8f7d7626f74039bc5ee
Vadim Igumnov <vad.igumnoff@gmail.com>
2024-02-21 13:21:55 +0300

    config: add "@" alias

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,6 +1,7 @@
 import { ResolveOptions } from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildResolvers(): ResolveOptions {
+export function buildResolvers({ paths }: BuildOptions): ResolveOptions {
   return {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
@@ -10,7 +11,8 @@ export function buildResolvers(): ResolveOptions {
        * поэтому нам нужно псевдоним vue для его runtime bundler.
        * @url https://james.pawsforthorpe.co.uk/posts/2022/02/webpack-typescript-vue.html
       */
-      'vue': 'vue/dist/vue.esm-bundler.js'
+      'vue': 'vue/dist/vue.esm-bundler.js',
+      '@': paths.src
     }
   }
 }
