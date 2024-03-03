d11a5eab2628f6caaebac26cc5e9f252a058da0a
Vadim Igumnov <vad.igumnoff@gmail.com>
2024-03-03 16:13:56 +0300

    config: setup report for Loki

---
+++ .loki/scripts/generate-visual-json-report.js
@@ -0,0 +1,28 @@
+const { promisify } = require('node:util')
+const { readdir, writeFile } = require('node:fs')
+const { join: joinPath, relative } = require('node:path')
+
+const asyncReaddir = promisify(readdir)
+const writeFileAsync = promisify(writeFile)
+
+const lokiDir = joinPath(__dirname, '..')
+const actualDir = joinPath(lokiDir, 'current')
+const expectedDir = joinPath(lokiDir, 'reference')
+const diffDir = joinPath(lokiDir, 'difference');
+
+(async function main() {
+  const diffs = await asyncReaddir(diffDir)
+
+  await writeFileAsync(joinPath(lokiDir, 'report.json'), JSON.stringify({
+    newItems: [],
+    deletedItems: [],
+    passedItems: [],
+    failedItems: diffs,
+    expectedItems: diffs,
+    actualItems: diffs,
+    diffItems: diffs,
+    actualDir: relative(lokiDir, actualDir),
+    expectedDir: relative(lokiDir, expectedDir),
+    diffDir: relative(lokiDir, diffDir),
+  }))
+})()
